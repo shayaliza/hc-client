80e105b2eea4df601d1cf2b4e221acb2bbf5a9a4
shayaliza <magicalmind01@gmail.comgit statuss>
2023-11-01 21:19:49 +0530

    home page corousal added

---
+++ app/(pages)/lol/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <Image src="/b1.jpg" width={500} height={500} alt="Picture of the author" />
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,23 +1,28 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
-// const items = ["Item 1", "Item 2", "Item 3", "Item 4"];
 const items = [
   {
-    text: "Item 1",
-    imageUrl: "https://dummyimage.com/1000x600/000/fff",
+    imageUrl: "/b1.jpg",
   },
   {
-    text: "Item 2",
-    imageUrl: "https://dummyimage.com/1000x600/000/fff",
+    imageUrl: "/b2.jpg",
   },
   {
-    text: "Item 3",
-    imageUrl: "https://dummyimage.com/1000x600/000/fff",
+    imageUrl: "/b3.jpg",
   },
   {
-    text: "Item 4",
-    imageUrl: "https://dummyimage.com/1000x600/000/fff",
+    imageUrl: "/b4.jpg",
+  },
+  {
+    imageUrl: "/b5.jpg",
+  },
+  {
+    imageUrl: "/b6.jpg",
+  },
+  {
+    imageUrl: "/b7.jpg",
   },
 ];
 
@@ -38,7 +43,7 @@ export default function Home() {
   }, [activeIndex]);
 
   return (
-    <div className="relative">
+    <div className="relative ">
       {items.map((item, index) => (
         <div
           key={index}
@@ -47,28 +52,35 @@ export default function Home() {
           }`}
         >
           <div className="bg-c2 h-64 m-4 p-4 rounded-lg shadow-md">
-            <img
+            <Image
               src={item.imageUrl}
-              alt={item.text}
+              width={1000}
+              height={600}
+              alt="Picture of the author"
               className="mb-2 rounded-full"
             />
-            {item.text}
           </div>
         </div>
       ))}
 
       <button
-        className="absolute top-1/2 left-2 transform -translate-y-1/2 text-gray-600 hover:text-gray-900"
+        className="absolute top-60 left-10  text-gray-600 hover:text-gray-900  text-6xl"
         onClick={prevItem}
       >
         &#8249;
       </button>
       <button
-        className="absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-600 hover:text-gray-900"
+        className="absolute top-60 right-10 text-6xl  text-gray-600 hover:text-gray-900"
         onClick={nextItem}
       >
         &#8250;
       </button>
+      {/* <button
+        className="absolute top-60 right-10 text-6xl transform -translate-y-1/2 text-gray-600 hover:text-gray-900"
+        onClick={nextItem}
+      >
+        &#8250;
+      </button> */}
     </div>
   );
 }
